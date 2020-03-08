465accceef79396a73bb46c63dc9871edf577a91
markv58 <markvittes@gmail.com>
2020-03-07 22:25:31 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 python3 -m pip install -r requirements.txt --user
+sudo apt-get install libbluetooth-dev
+sudo apt-get install python-dev
 sudo apt-get install -y pi-bluetooth bluez python-bluez
